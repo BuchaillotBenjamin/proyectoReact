fac97977c784d4f8d60edb442118572d5e495deb
BuchaillotBenjamin <146379236+BuchaillotBenjamin@users.noreply.github.com>
2024-11-14 11:53:20 -0300

    video 5 terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
 import MiPrimerComponent from "./components/MiprimerComponent/MiprimerComponent"
+import { ComponentUseEffect } from "./components/ComponentUseEfect/ComponentUseEfect"
 
 
 export const App = () => {
 
+
+
   return (
-    <>
+
+    < div style={{display: 'flex', flexDirection:'column', gap: '2vh' }}>
         
         <MiPrimerComponent text={"texto desde propiedad"} color={"red"} fontSize={5}/>
         <ComponentCounter/>
+        <ComponentUseEffect/>
 
-    </>
+    </div>
+    
   )
 
 }
